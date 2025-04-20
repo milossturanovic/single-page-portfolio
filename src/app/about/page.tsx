01d73074013e422b4090513c7246cde108bd0230
Miki <sturanovic.ms@gmail.com>
2025-04-20 17:17:58 +0200

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-4xl font-bold mb-6">About Me</h1>
+      <div className="prose lg:prose-xl">
+        <p>This is my about page. You can add your content here.</p>
+      </div>
+    </div>
+  );
+} 
