b6b4dc8040695c7f45eb279549ed128c1b496cc0
Miki <sturanovic.ms@gmail.com>
2025-05-05 18:09:13 +0200

    added the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { TapeSection } from "@/sections/Tape";
 import { TestimonialsSection } from "@/sections/Testimonials";
 import { AboutSection } from "@/sections/About";
 import { ContactSection } from "@/sections/Contact";
-
+import { Footer } from "@/sections/Footer";
 
 
 
@@ -19,6 +19,7 @@ export default function Home() {
       <TestimonialsSection /> 
       <AboutSection />
       <ContactSection />
+      <Footer />
     </div>
   );
 }
